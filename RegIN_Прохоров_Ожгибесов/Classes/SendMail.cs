66a07b6e801698c42238d7a9fac89cfdf2203549
AlonovArt <lzatos.off@gmail.com>
2025-11-28 21:05:48 +0500

    Разработанный класс отправки сообщения на почту. Прохоров

---
+++ RegIN_Прохоров_Ожгибесов/Classes/SendMail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegIN_Прохоров_Ожгибесов.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message,string To)
+        {
+            var smptClient = new smptClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credential = new NetworkCredential("yandex@yandex.ru", "password"),
+                EnableSsl = true,
+            };
+            smptClient.Send("landaxer.yandex.ru",To, "Проект RegIn",Message);
+        }
+    }
+}
