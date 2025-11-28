bfe42359a375d5ff6cba26eaec3a32c4433704b2
AlonovArt <lzatos.off@gmail.com>
2025-11-28 21:30:49 +0500

    Разработанный класс User. Прохоров

--- RegIN_Прохоров_Ожгибесов/Classes/SendMail.cs
+++ RegIN_Прохоров_Ожгибесов/Classes/SendMail.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,10 @@ namespace RegIN_Прохоров_Ожгибесов.Classes
     {
         public static void SendMessage(string Message,string To)
         {
-            var smptClient = new smptClient("smtp.yandex.ru")
+            var smptClient = new SmptClient("smtp.yandex.ru")
             {
                 Port = 587,
-                Credential = new NetworkCredential("yandex@yandex.ru", "password"),
+                Credential = new NetworkCredential("brenovc@yandex.ru", "pqstsmgvacsuqqxk"),
                 EnableSsl = true,
             };
             smptClient.Send("landaxer.yandex.ru",To, "Проект RegIn",Message);
