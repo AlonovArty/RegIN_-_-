dc91e5c11e10fb34b679a41f9b457e25976a4a59
AlonovArt <lzatos.off@gmail.com>
2025-12-01 00:10:45 +0500

    Программирование окна MainWindow. Прохоров

--- RegIN_Прохоров_Ожгибесов/MainWindow.xaml.cs
+++ RegIN_Прохоров_Ожгибесов/MainWindow.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RegIN_Прохоров_Ожгибесов.Classes;
 
 namespace RegIN_Прохоров_Ожгибесов
 {
@@ -20,9 +22,32 @@ namespace RegIN_Прохоров_Ожгибесов
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public User UserLogIn = new User();
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            OpenPage(new Pages.Login());
+        }
+
+
+        public void OpenPage (Page page)
+        {
+            DoubleAnimation StartAnimation = new DoubleAnimation();
+            StartAnimation.From = 1;
+            StartAnimation.To = 0;
+            StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            StartAnimation.Completed += delegate
+            {
+                frame.Navigate(page);
+                DoubleAnimation EndAnimation = new DoubleAnimation();
+                EndAnimation.From = 0;
+                EndAnimation.To = 1;
+                EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityProperty, EndAnimation);
+            };
+            frame.BeginAnimation(Frame.OpacityProperty,StartAnimation);
         }
     }
 }
