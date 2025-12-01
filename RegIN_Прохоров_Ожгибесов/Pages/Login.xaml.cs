212ce12aec7d41bbc75017f24a8029e7bebff9f9
AlonovArt <lzatos.off@gmail.com>
2025-12-01 20:50:35 +0500

    сделан шаг 31

--- RegIN_Прохоров_Ожгибесов/Pages/Login.xaml.cs
+++ RegIN_Прохоров_Ожгибесов/Pages/Login.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RegIN_Прохоров_Ожгибесов.Classes;
 
 namespace RegIN_Прохоров_Ожгибесов.Pages
 {
@@ -29,6 +31,9 @@ namespace RegIN_Прохоров_Ожгибесов.Pages
         public Login()
         {
             InitializeComponent();
+            MainWindow.mainWindow.UserLogIn.HandelCorrectLogin += CorrectLogin;
+            MainWindow.mainWindow.UserLogIn.HandlerInCorrectLogin += InCorrectLogin;
+            Capture.HandlerCorrectCapture += CorrectCapture;
         }
         public void CorrectLogin()
         {
@@ -66,12 +71,77 @@ namespace RegIN_Прохоров_Ожгибесов.Pages
                 OldLogin = TbLogin.Text;
             }
         }
+        public void InCorrectLogin()
+        {
+            if(LNameUser.Content != "")
+            {
+                LNameUser.Content = "";
+                DoubleAnimation StartAnimation = new DoubleAnimation();
+                StartAnimation.From = 1;
+                StartAnimation.To = 0;
+                StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                StartAnimation.Completed += delegate
+                {
+                    IUser.Source = new BitmapImage(new Uri("pack://application:,,,/Images/users.png"));
+                    DoubleAnimation EndAnimation = new DoubleAnimation();
+                    EndAnimation.From = 0;
+                    EndAnimation.To = 1;
+                    EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                    IUser.BeginAnimation(Image.OpacityProperty, EndAnimation);
+                };
+                 IUser.BeginAnimation(Image.OpacityProperty, StartAnimation);
+            }
+
+            if(TbLogin.Text.Length > 0)
+            {
+                SetNotification("Login is incorrect", Brushes.Red);
+            }
+        }
+        public void CorrectCapture()
+        {
+            Capture.IsEnabled = false;
+            IsCapture = true;
+        }
 
         private void SetPassword(object sender, KeyEventArgs e)
         {
-
+            if (e.Key == Key.Enter)
+            {
+                SetPassword();
+            }
         }
 
+        public void SetPassword()
+        {
+            if(MainWindow.mainWindow.UserLogIn.Password != String.Empty)
+            {
+                if (IsCapture)
+                {
+                    if(MainWindow.mainWindow.UserLogIn.Password == TbPassword.Password)
+                    {
+                        MainWindow.mainWindow.OpenPage(new Confirmation.TypeConfirmation.Login));
+                    }
+                    else
+                    {
+                        if(CountSetPassword > 0)
+                        {
+                            SetNotification($"Password is incorrect, {CountSetPassword} attempts left", Brushes.Red);
+                            CountSetPassword--;
+                        }
+                        else
+                        {
+                            Thread TBlockAutorization = new Thread(BlockAutorization);
+                            TBlockAutorization.Start();
+                        }
+                        SendMail.SendMessage("An attempt was made to log into your account", MainWindow.mainWindow.UserLogIn.Login);
+                    }
+                }
+            }
+            else
+            {
+                SetNotification($"Enter capture", Brushes.Red);
+            }
+        }
         private void OpenRegin(object sender, MouseButtonEventArgs e)
         {
 
